8809954688d95e842e9d03a6aaa05ecd4919be9d
Renato <renatomarquesteles@gmail.com>
2022-07-01 21:13:37 -0300

    adds new task form styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { PlusCircle } from 'phosphor-react';
+
 import { Header } from './components/Header';
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -6,6 +9,15 @@ export const App = () => {
   return (
     <div>
       <Header />
+
+      <main className={styles.content}>
+        <form className={styles.newTaskForm}>
+          <input type="text" placeholder="Add a new task" />
+          <button type="submit">
+            Create <PlusCircle size={20} />
+          </button>
+        </form>
+      </main>
     </div>
   );
 };
